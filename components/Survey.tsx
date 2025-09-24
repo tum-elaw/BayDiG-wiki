946e0d5a1e8bb0d3e7f014629846463c139056c0
Michael Bressler <53796824+mibressler@users.noreply.github.com>
2025-09-24 14:32:16 +0200

    Update Survey.tsx

--- components/Survey.tsx
+++ components/Survey.tsx
@@ -22,7 +22,14 @@ export const Survey = () => {
 
     
       </h2>
-      <p className="color-text-secondary f6">Das Wiki zum Bayerischen Digitalgesetz. Zu den Projektbeteiligten gehörten Prof. Dr. Dirk Heckmann (Leitung), Michael Bressler (Plattform), Alexander Besner und Nicolas Ziegler (Seminar)</p>
+      <p className='color-text-secondary f6'><b>Durch das BayDiG-Wiki schafft das CDPS einen wertvollen Beitrag zur vertieften demokratischen Diskussion der Landesgesetzgebung rund um das Bayerische Digitalgesetz. </b>
+Die quelloffene Plattform präsentiert die Arbeitsergebnisse eines Seminars, in dem sich die Studierenden der Technischen Universität München intensiv mit dem neuen Digitalgesetz auseinadersetzten.</p>
+<p className='color-text-secondary f6'>
+Durch die „Open Access“-Publizierung der studentischen Kommentierungen auf der eigens entwickelten Wiki-Plattform erhalten Interessierte ein umfangreiches Informationsangebot, welches das im Netz verfügbare Wissen bedeutend erweitert. </p>
+<p className='color-text-secondary f6'>Die fundierten Beiträge der Studierenden fanden ihr Lob auch durch die <b>Bayerische Staatsministerin für Digitales Judith Gerlach, MdL</b>, welche den Abschlussterminen einschließlich der Veröffentlichung der Seite am 28. März 2022 sowie am 14. März 2023 beisaß.</p>
+<p className='color-text-secondary f6'><a href="https://www.tum-cdps.de/2022/03/28/wiki-fuer-das-bayerische-digitalgesetz/">Bericht vom 28. März 2022 (Wintersemester 2021/22)</a></p>
+<p className='color-text-secondary f6'><a href="https://www.tum-cdps.de/2023/03/14/erweiterung-des-wikis-fuer-das-bayerische-digitalgesetz/">Bericht vom 14. März 2023 (Wintersemester 2022/23)</a></p>
+      <p className="color-text-secondary f6">Zu den Projektbeteiligten gehörten <a href="https://www.gov.sot.tum.de/elaw/lehrstuhlinhaber/">Prof. Dr. Dirk Heckmann</a> (Direktor, TUM CDPS), <a href="https://github.com/mibressler">Michael Bressler</a> (Plattform), <a href="https://www.gov.sot.tum.de/elaw/team/ehemalige/">Alexander Besner-Lettenbauer</a> und <a href="https://www.gov.sot.tum.de/elaw/team/nicolas-ziegler/">Nicolas Ziegler</a> (Seminar)</p>
 
       {/* Honeypot: token isn't a real field */}
       
