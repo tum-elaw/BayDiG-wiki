c11dfda6cd5cc308558ee8449fad6db22887d1b1
Michael Bressler <53796824+mibressler@users.noreply.github.com>
2025-09-24 14:56:14 +0200

    Archivierung der Projektseite

--- components/Support.tsx
+++ components/Support.tsx
@@ -10,11 +10,11 @@ export const Support = () => {
   return (
     <div>
       <h3 className="mb-2 f4">{t`still_need_help`}</h3>
-      <a id="ask-community" href="" target="_blank" className="btn btn-outline mr-4 mt-2 disabled">
+      <a id="ask-community" target="_blank" className="btn btn-outline mr-4 mt-2 disabled">
         <PeopleIcon size="small" className="octicon mr-1" />
         {t`ask_community`}
       </a>
-      <a id="qna" href="" target="_blank" className="btn btn-outline mr-4 mt-2 disabled">
+      <a id="qna" target="_blank" className="btn btn-outline mr-4 mt-2 disabled">
         <QuestionIcon size="small" className="octicon mr-1" />
         {t`qna`}
       </a>
